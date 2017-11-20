c67dfddb9b33cf8a33ba89afd1d9f88cafd5987c
Ramcharan Chauhan <srs0447@gmail.com>
2017-11-21 01:34:34 +0530

    I add a new file to project

---
+++ 9-list.py
@@ -0,0 +1,6 @@
+# printing the first and last color from a list
+
+# defining the list
+
+color_list = ["Red","Green","White" ,"Black"]
+print("First color is: ", color_list[0], "Last color is: ", color_list[3])
